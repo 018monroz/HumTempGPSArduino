9bc5b24cb13d20be65512703efee126b994573fb
018monroz <juan.montoyao@upb.edu.co>
2023-09-08 15:08:07 -0500

    Limpieza y documentacion

--- src/main.cpp
+++ src/main.cpp
@@ -15,23 +15,20 @@ const char* password = "";
 
 const char* server = "54.225.72.244";
 
-/* This sample code demonstrates the normal use of a TinyGPS object.
-   It requires the use of SoftwareSerial, and assumes that you have a
-   4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
-*/
 
 ClosedCube_HDC1080 sensor;
-double i;
-double sumTemp;
-double sumHum;
+
+
+
+//Conteo de iteraciones
 int countGPS;
-int countH;
 int countT;
 
+//Objeto GPS
 TinyGPS gps;
 SoftwareSerial ss(2, 0);
 
-//Datos a enviar
+//Datos para podado y envío
 double latitud;
 double longitud;
 double temperaturaEnv;
@@ -39,67 +36,14 @@ double temperaturaSum;
 double humedadEnv;
 double humedadSum;
 
-int id = 1001;
 
 WiFiClient client;
 
 //Vbles lectura gps
 static void smartdelay(unsigned long ms);
 static void print_float(float val, float invalid, int len, int prec);
-static void print_int(unsigned long val, unsigned long invalid, int len);
-static void print_date(TinyGPS &gps);
-static void print_str(const char *str, int len);
-static String fecha();
-static String hora();
 
-/*static void sendthingspeak()
-{
-
-  String PostData = "";
-
-  Serial.println("datos para enviar");
-
-  PostData = String("{\"id\":"+String(id)+", \"temperatura\":"+String(temperatura,7)+", \"longitud\":"+String(longitud,7)+", \"latitud\":"+String(latitud,7)+"}");
-
-  Serial.println(PostData);
-
-  if ( client.connect(server,80))
-
-  {
-
-    Serial.println("conectado");
-
-    client.print("POST /sensor HTTP/1.1\n");
-
-    // poner la direccion IP del servidor ​
-
-    client.print("Host: 18.212.146.249 \n"); //el de ander
-
-    //client.println("User-Agent: Arduino/1.0");
-
-    //client.println("Connection: close");
-
-    client.println("Content-Type: application/json;"); //Cambiar a JSON
-
-    client.print("Content-Length: ");
-
-    client.println(PostData.length());
-
-    client.println();
-
-    client.println(PostData);
-
-  } else {
-
-    Serial.println("error de conexion");
-
-  }
-
-}
-*/
-
-
-//Formato mensaje a cambiar según requerimientos del servidor.
+//Envio de datos
 static void EnvioHumedadyTemperatura() 
 {
 
@@ -107,6 +51,7 @@ static void EnvioHumedadyTemperatura()
 
   Serial.println("datos para enviar");
 
+  //Formato en JSON por petición del servidor
   PostData = String("{\"id\":\"000420108\", \"temperatura\":\""+String(temperaturaEnv,7)+"\", \"humedad\":\""+String(humedadEnv,7)+"\"}");
 
   Serial.println(PostData);
@@ -117,9 +62,8 @@ static void EnvioHumedadyTemperatura()
 
     Serial.println("conectado");
 
+    //Trama HTTP
     client.print("POST /datos HTTP/1.1\n");
-
-    // poner la direccion IP del servidor ​
 
     client.print("Host:  54.225.72.244 \n"); //Serv destino
 
@@ -192,6 +136,7 @@ PostData = String("{\"id\":\"000420108\", \"latitud\":\""+String(latitud,7)+"\",
 
 void setup()
 { 
+  //Arranque dispositivos
   Serial.begin(115200);
   ss.begin(9600);
   sensor.begin(0x40);
@@ -203,7 +148,8 @@ void setup()
   Serial.print("Connecting to ");
 
   Serial.println(ssid);
-
+  
+  //Conexión a Red
 WiFi.begin(ssid, password);
 
 while (WiFi.status() != WL_CONNECTED) {
@@ -218,12 +164,8 @@ while (WiFi.status() != WL_CONNECTED) {
 
  Serial.println("WiFi connected");
 
-  //vbles temperatura pruning
-  i = 0.0;
-  sumTemp = 0.0;
-  sumHum = 0.0;
+  //Inicialización de variables
   countGPS = 0;
-  countH = 0;
   countT = 0;
   temperaturaSum = 0;
   temperaturaEnv = 0;
@@ -233,14 +175,14 @@ while (WiFi.status() != WL_CONNECTED) {
 
 void loop()
 {
-  countGPS++;
-  float flat, flon;
+  countGPS++; //Cada count significa un lapso de 100ms
+  float flat, flon;  //Para la captura de gps desde la librería
   unsigned long age, date, time, chars = 0;
   unsigned short sentences = 0, failed = 0;
   static const double LONDON_LAT = 51.508131, LONDON_LON = -0.128002;
 
 
-  if(countGPS > 100){ //Se envia GPS
+  if(countGPS > 100){ //Pasaron 100x100ms. Se envia GPS (10 segundos)
 
     gps.f_get_position(&flat, &flon, &age);
     latitud = flat;
@@ -250,7 +192,7 @@ void loop()
     //Envío Paquete.
     EnvioGPS();
 
-    //Obtención de datos
+    //Obtención de datos de humedad y temperatura
 
     temperaturaSum += sensor.readTemperature();
     humedadSum += sensor.readHumidity();
@@ -261,7 +203,7 @@ void loop()
   
   if(countT == 3){ //30 segundos, se envía H, T
 
-  //Prunning:
+  //Prunning: Se promedian los datos para enviar los correctos.
   temperaturaEnv = temperaturaSum/3.0;
   humedadEnv = humedadSum/3.0;
 
@@ -279,6 +221,7 @@ void loop()
   smartdelay(100);
 }
 
+//Delay en el que se puede capturar GPS
 static void smartdelay(unsigned long ms)
 {
   unsigned long start = millis();
@@ -287,84 +230,5 @@ static void smartdelay(unsigned long ms)
     while (ss.available())
       gps.encode(ss.read());
   } while (millis() - start < ms);
-}
-
-static void print_float(float val, float invalid, int len, int prec)
-{
-  if (val == invalid)
-  {
-    while (len-- > 1)
-      Serial.print('*');
-    Serial.print(' ');
-  }
-  else
-  {
-    Serial.print(val, prec);
-    int vi = abs((int)val);
-    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
-    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
-    for (int i=flen; i<len; ++i)
-      Serial.print(' ');
-  }
-  smartdelay(0);
-}
-
-static void print_int(unsigned long val, unsigned long invalid, int len)
-{
-  char sz[32];
-  if (val == invalid)
-    strcpy(sz, "*******");
-  else
-    sprintf(sz, "%ld", val);
-  sz[len] = 0;
-  for (int i=strlen(sz); i<len; ++i)
-    sz[i] = ' ';
-  if (len > 0) 
-    sz[len-1] = ' ';
-  Serial.print(sz);
-  smartdelay(0);
-}
-
-static void print_date(TinyGPS &gps)
-{
-  int year;
-  byte month, day, hour, minute, second, hundredths;
-  unsigned long age;
-  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
-  if (age == TinyGPS::GPS_INVALID_AGE)
-    Serial.print("********** ******** ");
-  else
-  {
-    char sz[32];
-    sprintf(sz, "%02d/%02d/%02d %02d:%02d:%02d ",
-        month, day, year, hour, minute, second);
-    Serial.print(sz);
-  }
-  print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
-  smartdelay(0);
-}
-
-static String fecha(){
-  int year;
-  byte month, day, hour, minute, second, hundredths;
-  unsigned long age;
-  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
-  return ""+String(day)+"/"+String(month)+"/"+String(year);
-}
-
-static String hora(){
-  int year;
-  byte month, day, hour, minute, second, hundredths;
-  unsigned long age;
-  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
-  return ""+String(hour)+":"+String(minute)+":"+String(second);
-}
-
-static void print_str(const char *str, int len)
-{
-  int slen = strlen(str);
-  for (int i=0; i<len; ++i)
-    Serial.print(i<slen ? str[i] : ' ');
-  smartdelay(0);
 }
 
